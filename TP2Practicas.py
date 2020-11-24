a1c3f743036c54d40118c00a06471f38f1906241
juanescu <juanescu02@hotmail.com>
2020-11-23 22:58:39 -0300

    archivo

---
+++ TP2Practicas.py
@@ -0,0 +1,58 @@
+print ("Ingrese 2 para calcular el maximo dentre 2 numeros")
+print ("Ingrese 3 para calcular el maximo dentre 3 numeros")
+print ("Ingrese 4 para calcular el largo de una lista")
+print ("Ingrese 5 para Determinar si un caracter ingresado es vocal o consonante")
+print ("Ingrese 6 para Realizar la suma de los elementos de una lista")
+print ("Ingrese 7 para Realizar la multiplicacion de los elementos de una lista")
+
+opcion = input()
+
+if opcion == 2: 
+	print ("Ingrese dos numeros")
+        n1= int (input())
+        n2= int (input())
+	if (n1 > n2):
+		print ("El mayor es: ", n1)
+	else:
+		print ("El mayor es: ", n2)
+	
+if opcion == 3:
+	print ("Ingrese 3 numeros")
+	n1= int (input())
+        n2= int (input())
+	n3= int (input())
+	if (n1 > n2) & (n1>n3):
+		print ("El mayor es: ", n1)
+	if (n2 > n1) & (n2>n3):
+		print ("El mayor es: ", n2)
+	if (n3 > n1) & (n3>n2):
+		print ("El mayor es: ", n3)
+
+if opcion == 4:
+	lista=[]
+	largo=0
+	print ("Ingrese un elemento de la lista o False para termimar de agregar")
+	i= bool (input ())
+	while (i):
+		print ("Ingrese un elemento de la lista")
+		elemento= input()
+		lista.append(elemento)
+		print ("Ingrese True para ingresar un elemento de la lista o False para termimar de agregar")
+		i= bool (input ())
+		largo= largo + 1
+	print ("El largo de la lista es: ", largo)
+	
+	
+if opcion == 5:
+	c =  str (input ("Ingrese un caracter"))
+	if c=="a" or c=="e" or c=="i" or c=="o"or c=="u" or c=="A"or c=="E"or c=="I"or c=="O"or c=="U":
+		print ("La letra ingresada es vocal")
+	else:
+		print ("La letra ingresada es consonante")
+
+
+
+
+
+	
+		
